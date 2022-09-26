52eb5c6696396c2f44a26092a047cbe62ab5c976
محمد محمد <softech.mohammadthedark333@gmail.com>
2022-09-26 16:16:34 +0300

    A profile page has been added and an edit has been added

--- dashboard/profile.php
+++ dashboard/profile.php
@@ -5,64 +5,63 @@
     if(!isset($_SESSION['adminInfo'])){
         header('Location:index.php');
     }
-else{
-    
+else{ 
 
-  ?>
+?>
 
-    <!-- /#sidebar-wrapper -->
+<!-- /#sidebar-wrapper -->
 
-    <!-- Page Content -->
+<!-- Page Content -->
 
-      <div class="container-fluid">
-       <?php 
+<div class="container-fluid">
+    <?php 
             $query = "SELECT * FROM admin";
             $result = mysqli_query($con,$query);
             $row = mysqli_fetch_assoc($result);
         ?>
-        
-                <?php
+
+    <?php
             if(isset($_POST['edit']))  {
-                $adminName = $_POST['adminName'];
-                $adminEmail = $_POST['adminEmail'];
-                $adminPass = $_POST['adminPass'];
+                $name = $_POST['name'];
+                $email = $_POST['email'];
+                $password = $_POST['password'];
                 
                 $query = "UPDATE admin SET
-                    adminName = '$adminName',
-                    adminEmail = '$adminEmail',
-                    adminPass = '$adminPass'
-                WHERE id = '1'
+                    name = '$name',
+                    email = '$email',
+                    password = '$password'
                 ";
                 $res = mysqli_query($con,$query);
                 header("REFRESH:0");
                 exit();
             }
         ?>
-        <div class="profile">
-            <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
-                <div class="form-group">
-                    <label for="name">الإسم</label>
-                    <input type="text" class="form-control" id="name" value="<?php  echo $row['adminName']; ?>" name="adminName">
-                </div>
-                <div class="form-group">
-                    <label for="email">البريد الإلكتروني</label>
-                    <input type="text" class="form-control" id="email"  value="<?php  echo $row['adminEmail']; ?>" name="adminEmail">
-                </div>
-                <div class="form-group">
-                    <label for="pass">كلمة السر</label>
-                    <input type="text" class="form-control" id="pass"  value="<?php  echo $row['adminPass']; ?>" name="adminPass">
-                </div>
-                <button class="custom-btn" name="edit">تعديل البيانات</button>
-            </form>
-        </div>
+    <div class="profile">
+        <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+            <div class="form-group">
+                <label for="name">الإسم</label>
+                <input type="text" class="form-control" id="name" value="<?php  echo $row['name']; ?>" name="name">
+            </div>
+            <div class="form-group">
+                <label for="email">البريد الإلكتروني</label>
+                <input type="text" class="form-control" id="email" value="<?php  echo $row['email']; ?>" name="email">
+            </div>
+            <div class="form-group">
+                <label for="pass">كلمة السر</label>
+                <input type="text" class="form-control" id="pass" value="<?php  echo $row['password']; ?>"
+                    name="password">
+            </div>
+            <button class="custom-btn" name="edit">تعديل البيانات</button>
+        </form>
     </div>
-    <!-- /#page-content-wrapper -->
+</div>
+<!-- /#page-content-wrapper -->
 
-  </div>
-  <!-- /#wrapper -->
+</div>
+<!-- /#wrapper -->
 <?php
-  include 'include/footer.php';
- ?>
+include 'include/footer.php';
+?>
 
 
 <?php 
