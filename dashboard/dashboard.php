8f7c1971ddb207d22c9dd056d55ae638cec7cdd0
محمد محمد <softech.mohammadthedark333@gmail.com>
2022-10-08 09:54:22 +0300

    An overview page has been added to display the number of books and display the number of categories

--- dashboard/dashboard.php
+++ dashboard/dashboard.php
@@ -3,26 +3,39 @@ session_start();
 include 'include/connection.php';
 include 'include/header.php';
 if (!isset($_SESSION['adminInfo'])) {
-  header('Location:index.php');
+    header('Location:index.php');
 } else {
 
 
 ?>
+
 <!-- /#sidebar-wrapper -->
+
 <!-- Page Content -->
+
 <div class="container-fluid">
     <div class="content">
         <div class="statistics text-center">
             <div class="row">
                 <div class="col-sm-6">
                     <div class="statistic">
-                        <h3></h3>
+                        <?php
+                            $query = "SELECT id FROM books";
+                            $result = mysqli_query($con, $query);
+                            $bookNum = mysqli_num_rows($result);
+                            ?>
+                        <h3><?php echo $bookNum; ?></h3>
                         <p>عدد الكتب</p>
                     </div>
                 </div>
                 <div class="col-sm-6">
                     <div class="statistic">
-                        <h3></h3>
+                        <?php
+                            $query = "SELECT id FROM categories";
+                            $result = mysqli_query($con, $query);
+                            $catNum = mysqli_num_rows($result);
+                            ?>
+                        <h3><?php echo $catNum; ?></h3>
                         <p>عدد التصنيفات</p>
                     </div>
                 </div>
