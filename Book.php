6621578eb2a406b9f1665de3b6c4dd82755979a5
محمد محمد <softech.mohammadthedark333@gmail.com>
2022-10-08 10:01:49 +0300

    A book detail page has been added and books are downloaded

--- Book.php
+++ Book.php
@@ -1,6 +1,10 @@
 <?php
+include 'dashboard/include/connection.php';
 include 'layout/include/header.php';
 
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+}
 ?>
 <!--    End navbar    -->
 
@@ -9,21 +13,25 @@ include 'layout/include/header.php';
     <div class="container">
         <div class="book">
             <div class="row">
-
+                <?php
+                $query = "SELECT * FROM books WHERE id='$id'";
+                $result = mysqli_query($con, $query);
+                $row = mysqli_fetch_assoc($result);
+                ?>
                 <div class="col-md-4">
                     <div class="book-cover">
-                        <img src="uploads\bookCovers/" alt=" Book cover">
+                        <img src="uploads\bookCovers/<?php echo $row['cover']; ?>" alt=" Book cover">
                     </div>
                 </div>
                 <div class="col-md-8">
                     <div class="book-content">
-                        <h4></h4>
+                        <h4><?php echo $row['title']; ?></h4>
                         <h5>
-                            <a href=""> </a>
-                        </h5>"
+                            <a href="author.php?author=<?php echo $row['author']; ?>"><?php echo $row['author']; ?></a>
+                        </h5>
                         <hr>
-                        <p></p>
-                        <button class=" custom-btn" style="width: 160px">
+                        <p><?php echo $row['content']; ?></p>
+                        <button class="custom-btn" style="width: 160px">
                             <a href="uploads\books/<?php echo $row['book']; ?>" download>تحميل الكتاب</a>
                         </button>
 
@@ -41,21 +49,33 @@ include 'layout/include/header.php';
         <h4>كتب ذات صلة</h4>
         <hr>
         <div class="row">
+            <?php 
+            if(isset($_GET['category'])){
+                $bookCat = $_GET['category'];
+            }
+            // fetch related books
+            $query = "SELECT * FROM books WHERE category = '$bookCat' AND id !='$id'";
+            $res = mysqli_query($con,$query);
+            while($row = mysqli_fetch_assoc($res)){
+            ?>
             <div class="col-lg-3 col-md-4 col-6">
                 <div class="related-book text-center">
                     <div class="cover">
-                        <a href="book.php">
-                            <img src="uploads/bookCovers/">
+                        <a href="book.php?id=<?php echo $row['id']; ?>&&category=<?php echo $row['category']; ?>">
+                            <img src="uploads/bookCovers/<?php echo $row['cover']; ?>" alt="Book Cover">
                         </a>
                     </div>
                     <div class="title">
                         <h5>
-                            <a href="book.php"></a>
+                            <a
+                                href="book.php?id=<?php echo $row['id']; ?>&&category=<?php echo $row['category'];?>"><?php echo $row['title']; ?></a>
                         </h5>
                     </div>
                 </div>
             </div>
-
+            <?php
+            }
+        ?>
         </div>
     </div>
 </div>
